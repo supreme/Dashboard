13e91a7aafe9cb8c3ef9056c5cae8e7c899b22ac
supreme <sandrews.r@gmail.com>
2016-12-07 18:54:45 -0500

    Sloppy but functional layout.

--- server.js
+++ server.js
@@ -9,17 +9,84 @@ let hbs = exphbs.create({
     defaultLayout: 'main',
     partialsDir: [
         'views/partials/'
-    ]
+    ],
+    helpers: {
+        compare: function (lvalue, operator, rvalue, options) {
+
+            var operators, result;
+
+            if (arguments.length < 3) {
+                throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+            }
+
+            if (options === undefined) {
+                options = rvalue;
+                rvalue = operator;
+                operator = "===";
+            }
+
+            operators = {
+                '==': function (l, r) { return l == r; },
+                '===': function (l, r) { return l === r; },
+                '!=': function (l, r) { return l != r; },
+                '!==': function (l, r) { return l !== r; },
+                '<': function (l, r) { return l < r; },
+                '>': function (l, r) { return l > r; },
+                '<=': function (l, r) { return l <= r; },
+                '>=': function (l, r) { return l >= r; },
+                'typeof': function (l, r) { return typeof l == r; }
+            };
+
+            if (!operators[operator]) {
+                throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+            }
+
+            result = operators[operator](lvalue, rvalue);
+
+            if (result) {
+                return options.fn(this);
+            } else {
+                return options.inverse(this);
+            }
+        }
+    }
 });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+let cards = [
+    {
+        'type': 'image',
+        'src': '../img/canvas_logo.svg'
+    },
+    {
+        'type': 'image',
+        'src': '../img/canvas_logo.svg'
+    },
+    {
+        'type': 'default',
+        'title': 'F',
+        'content': 'I got twoooooooo versions',
+        'updated-at': '7 mins ago'
+    },
+        {
+        'type': 'default',
+        'title': 'F',
+        'content': 'I got twoooooooo versions',
+        'updated-at': '7 mins ago'
+    },
+        {
+        'type': 'default',
+        'title': 'F',
+        'content': 'I got versions',
+        'updated-at': '7 mins ago'
+    }
+];
 //Setup routes
 app.get('/', (req, res) => {
   res.render('home', {
-    title: 'Test',
-    content: 'I am some content...'
+    'cards': cards
   });
 });
 
