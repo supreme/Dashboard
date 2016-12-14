4762d1511fff95698b76b7574d6a07ac4a522a26
supreme <sandrews.r@gmail.com>
2016-12-13 20:21:52 -0500

    Add documentation to campus event JS

--- app/public/js/manager.js
+++ app/public/js/manager.js
@@ -8,7 +8,7 @@
 /**
  * Compares today's date with the specified Unix time.
  * @param  {Integer}  date The date in Unix time.
- * @return {Boolean}      True if the specified date is today.
+ * @return {Boolean}       True if the specified date is today.
  */
 let isToday = (date) => {
   let today = new Date(Date.now());
@@ -21,11 +21,24 @@ let isToday = (date) => {
  * Event card
  * =======================================
  */
+
+/**
+ * Inteded to be used in a filter function to remove events not occuring today.
+ * @param  {Object} e A singular event.
+ * @return {Boolean}  True if the event date is today.
+ */
 let getTodaysEvents = (e) => {
   return isToday(e.date);
 }
 
-let renderEventCard = (events) => {
+/**
+ * Sorts through events that are occuring today and renders them to the
+ * campus events card. Appends each event to the event card formatted to
+ * a simple template.
+ * @param  {Object} events All of the events fetched from the /api/events.
+ */
+let displayCampusEvents = (events) => {
+  let todaysEvents = events.filter(getTodaysEvents)
   let content = $('#event-card .content');
   for (var e of events) {
     let template = `<div class="event">
@@ -37,11 +50,9 @@ let renderEventCard = (events) => {
   }
 }
 
-let displayEvents = (events) => {
-  let todaysEvents = events.filter(getTodaysEvents)
-  renderEventCard(todaysEvents);
-}
-
+/**
+ * AJAX request to fetch the list of campus events.
+ */
 $.ajax({
   type: "GET",
   url: '/api/events',
