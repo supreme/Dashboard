aeda26160903b0c22ebff4d4fd73dadd0cb3abbb
supreme <sandrews.r@gmail.com>
2016-12-14 16:20:31 -0500

    Add migrations

---
+++ app/migrations/20161211142935_create_tables.js
@@ -0,0 +1,29 @@
+let DEV_MODE = false;
+
+exports.up = (knex, Promise) => {
+      return Promise.all([
+        knex.schema.createTableIfNotExists('imagecards', (table) => {
+            table.increments();
+            table.string('image');
+            table.string('text');
+            table.string('url');
+            table.timestamps();
+        }),
+        knex.schema.createTableIfNotExists('events', (table) => {
+            table.increments();
+            table.integer('date');
+            table.string('image');
+            table.string('name');
+            table.string('org');
+            table.string('time');
+            table.timestamps();
+        })
+    ]);
+};
+
+exports.down = (knex, Promise) => {
+    if (DEV_MODE) {
+        knex.schema.dropTableIfExists('imagecards');
+        knex.schema.dropTableIfExists('events');
+    }
+};
